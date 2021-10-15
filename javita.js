a9406dc69dc4570f1a393cd043f458e5f8e3ae26
Nicolas <perroponcheyt@gmail.com>
2021-10-15 10:16:57 -0500

    commit obligatorio xd

---
+++ javita.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $('#button-addon2').click(function () { 
+       var gusto = $('#input_añadir').val();
+       
+        $('#tabla').append("<tr><td>Hola</td></tr>");
+       
+   });
+   
+
+});
